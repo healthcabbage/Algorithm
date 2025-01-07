51f1bc1af6e8612a7e8b74f5b983d78f66aeb89a
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2025-01-07 10:41:08 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = -1;
+    long long pay = 0;
+    
+    for (int i = 1; i <= count; i++)
+    {
+        pay += price * i;
+    }
+    
+    if (pay <= money)
+    {
+        answer = 0;
+    }
+    else
+    {
+        answer = pay - money;
+    }
+
+    return answer;
+}
