2b208153c31a6232280620966f225d2696fef070
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2025-01-20 09:27:43 +0900

    [level 1] Title: 시저 암호, Time: 0.40 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == ' ') continue;
+        else
+        {
+            if ('A' <= s[i] && s[i] <= 'Z')
+            {
+                s[i] += n - 26;
+                if (s[i] < 'A') 
+                {
+                    s[i] += 26;
+                }
+            }
+            else if('a' <= s[i] && s[i] <= 'z')
+            {
+                s[i] += n - 26;
+                if (s[i] < 'a') 
+                {
+                    s[i] += 26;
+                }
+            }
+        }
+        
+        answer = s;
+    }
+    
+    return answer;
+}
