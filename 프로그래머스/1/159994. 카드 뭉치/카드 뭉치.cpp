e88bb8538fede8856ae4e2f52a47f843c25d2df6
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2025-02-04 09:58:25 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -4,28 +4,6 @@
 using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
-    string answer = "Yes";
-    int x = 0;
-    int y = 0;
-    
-    for (int i = 0; i < goal.size(); i++)
-    {
-        string curword = goal[i];
-        
-        if (curword == cards1[x])
-        {
-            x++;
-        }
-        else if (curword == cards2[y])
-        {
-            y++;
-        }
-        else
-        {
-            answer = "No";
-            break;
-        }
-    }
-    
+    string answer = "";
     return answer;
 }
