4f82b606a754dd14801a6c2695002d7d5b2dff72
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2026-09-17 11:58:22 +0900

    [Passed] 변수 값 교체하기 6 (2ms, 0MB)

---
+++ trail1/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 2;
+    int b = 5;
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << a << "\n" << b;
+    return 0;
+}
