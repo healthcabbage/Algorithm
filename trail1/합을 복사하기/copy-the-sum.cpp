df22065ed95a62b31e563bded7c19c6f947dd045
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2026-09-17 19:50:50 +0900

    [Passed] 합을 복사하기 (2ms, 0MB)

---
+++ trail1/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    a = a + b + c;
+
+    c = b = a;
+
+    cout << a << " " << b << " " << c;
+    return 0;
+}
