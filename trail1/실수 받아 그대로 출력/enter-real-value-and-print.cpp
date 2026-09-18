2f404e596ecf21e2b2e36292f3239e87344f13a6
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2026-09-18 10:04:40 +0900

    [Passed] 실수 받아 그대로 출력 (2ms, 0MB)

---
+++ trail1/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    double a;
+    cin >> a;
+
+    cout.precision(2);
+    cout << a;
+    return 0;
+}
