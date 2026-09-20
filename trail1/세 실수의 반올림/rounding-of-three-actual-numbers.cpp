d0b98041e13dd0d3e7bf4c2b4537c840c59fe61c
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2026-09-21 08:31:05 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ trail1/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout.precision(3);
+    cout << a << "\n" << b << "\n" << c;
+    return 0;
+}
