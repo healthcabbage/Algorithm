bf99fe8d07e12d5c2d44dddfaaf4338a8feee14e
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2026-09-23 09:22:17 +0900

    [Passed] 문자열 순서 바꾸기 (2ms, 0MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string S, T;
+
+    cin >> S >> T;
+
+    cout << T << endl;
+    cout << S << endl;
+    return 0;
+}
