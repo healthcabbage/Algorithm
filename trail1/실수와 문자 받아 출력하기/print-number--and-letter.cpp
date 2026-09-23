726eaee456b34410866b9dd7258bd2dce2633048
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2026-09-23 09:15:36 +0900

    [Passed] 실수와 문자 받아 출력하기 (2ms, 0MB)

---
+++ trail1/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    double a, b;
+    char c;
+
+    cin >> c >> a >> b;
+    cout.precision(2);
+    cout << c << endl;
+    cout << a << endl;
+    cout << b << endl;
+    return 0;
+}
