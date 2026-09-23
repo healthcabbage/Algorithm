e4b3f5ec0bdb55304e793c29aa28eff072dcbd8e
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2026-09-23 11:20:17 +0900

    [Passed] 날짜 변경하여 출력 2 (2ms, 0MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+
+    cin >> c;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> a;
+
+    cout << a << "." << c << "." << b;
+    return 0;
+}
